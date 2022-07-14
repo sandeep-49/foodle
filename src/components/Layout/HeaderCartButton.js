2f015ae788fd2d2e0a8ba98b225e8f56458b8404
Sandeep Sharma <sandeepsharma.4994@gmail.com>
2022-07-14 18:27:17 +0530

    Add cart items

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,35 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart-context';
 
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = props => {
+	const [btnHighlight, setBtnHighlight] = useState(false);
 	const cartCtx = useContext(CartContext);
 
-	const numberOfCartItems = cartCtx.items.reduce((currNumber, item) => {
+	const { items } = cartCtx;
+
+	const numberOfCartItems = items.reduce((currNumber, item) => {
 		return currNumber + item.amount;
 	}, 0);
 
+	const btnClasses = `${classes.button} ${btnHighlight ? classes.bump : ''}`;
+
+	useEffect(() => {
+		if (items.length === 0) return;
+
+		setBtnHighlight(true);
+
+		const timer = setTimeout(() => setBtnHighlight(false), 300);
+
+		return () => {
+			clearTimeout(timer);
+		};
+	}, [items]);
+
 	return (
-		<button className={classes.button} onClick={props.onClick}>
+		<button className={btnClasses} onClick={props.onClick}>
 			<span className={classes.icon}>
 				<CartIcon />
 			</span>
